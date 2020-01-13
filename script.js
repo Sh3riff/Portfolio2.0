d4bd8452ec05287e1f6f64a919289db3db538327
sh3riff <contactme@sh3riff.me>
2020-01-13 14:58:13 +0100

    date and time function added

--- script.js
+++ script.js
@@ -16,6 +16,43 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 
+    (function showTime (){
+        let timeNow = new Date();
+        const addZeroToTime = (m) => {
+            if(m < 10){
+                return '0'+m
+            }else {
+                return m
+            }
+        };
+        const aZ = addZeroToTime;
+        const hourFormat = (hr) => {
+            let m = hr % 12 || 12;
+            if(m < 10){
+                return '0'+m
+            }else {
+                return m
+            }      
+        };
+        const hr = hourFormat;
+        const traytime = document.querySelector('.traytime')
+        const maintime = document.querySelector('.maintime')
+        const date = document.querySelector('.date')
+        let min = timeNow.getMinutes();
+        let hour = timeNow.getHours();
+        let hrMin = `${hr(hour)}:${aZ(min)}`      
+        let fulldate = timeNow.toDateString();
+        let fulltime = timeNow.toLocaleTimeString();
+        
+        traytime.innerHTML = `${hrMin}`;
+        maintime.innerHTML = `${fulltime}`;
+        date.innerHTML = `${fulldate}`;
+        
+        // time.innerHTML = `${hour}<span>:</span>${aZ(min)}<span>:</span>${aZ(sec)}`;
+
+        setTimeout(showTime, 1000);
+    }());
+
 
     
 })
